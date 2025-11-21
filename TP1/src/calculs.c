e10ffffbde95832fa35a16a71fbaa54c9565da83
Space V <achidal@proton.me>
2025-11-21 10:32:37 +0100

    feat(tp1/calculs): add calculs

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 4;
+    char op = '&';
+
+    switch (op) {
+        case '+':
+            printf("Resultat = %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Resultat = %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Resultat = %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Resultat = %d\n", num1 / num2);
+            else
+                printf("Erreur : division par zero\n");
+            break;
+
+        case '%':
+            if (num2 != 0)
+                printf("Resultat = %d\n", num1 % num2);
+            else
+                printf("Erreur : modulo par zero\n");
+            break;
+
+        case '&':
+            printf("Resultat = %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("Resultat = %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("Resultat = %d\n", ~num1);
+            break;
+
+        default:
+            printf("Operateur inconnu\n");
+    }
+
+    return 0;
+}
 
