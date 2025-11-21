821d139aeeaa82631c54e0ca6bc89fae31debd02
root <root@Ahcene.localdomain>
2025-11-21 10:08:06 +0100

    refactor(tp1/cercle): calculate perimeter of the circle

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main() {
-double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
-}
+   double rayon = 6.0;
+   double aire = M_PI * rayon * rayon;
+   double perimetre = 2 * M_PI * rayon;
 
+
+   printf("Rayon du cercle : %.2f\n", rayon);
+   printf("Aire du cercle : %.4f\n", aire);
+   printf("Perimetre du cercle : %.4f\n", perimetre);
+}
