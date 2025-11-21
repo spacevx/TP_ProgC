0545675b60b575521019d9723a2895eb4921df29
Space V <achidal@proton.me>
2025-11-21 10:23:36 +0100

    feat(tp1/operateurs): add operateurs

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+
+    printf("a == b : %d\n", a == b);
+    printf("a > b  : %d\n", a > b);
+
+    return 0;
+}
